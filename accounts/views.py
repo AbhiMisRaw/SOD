96f2bc8b345905b97e529ad8ef914da8fe71a7ec
sameershaik-coder <sameershaik.coder@gmail.com>
2024-01-13 20:26:03 +0530

    cleaned namespaces

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-from core.users_api_service import register_user,login_user,logout_user
-from django.contrib.auth import authenticate
-from django.contrib.auth import login
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from core.users_api_service import register_user, login_user, logout_user
 from django.contrib import messages
-from django.contrib.auth import logout
-from django.shortcuts import redirect
 
 def logout_view(request):
-    data={}
-    #data['email'] = request.session['email'] 
+    data = {}
     data['token'] = request.session['token']
     logout_user(data)
     request.session['token'] = None
     return redirect('login')
-
-
 
 def register_view(request):
     if request.method == 'POST':
@@ -27,18 +17,14 @@ def register_view(request):
             # Add more fields as needed
         }
         response = register_user(data)
-        if response['status'] == 'success':  
+        if response['status'] == 'success':
             request.session['email'] = data['email']
-            # If the API returns a token, you can store it in the session as well
             request.session['token'] = response.get('token')
             return redirect('home')
         else:
-            # If the registration fails, you can show an error message
             messages.error(request, 'Registration failed. Please try again.')
     else:
         return render(request, 'accounts/register.html')
-
-
 
 def login_view(request):
     if request.method == 'POST':
@@ -48,10 +34,8 @@ def login_view(request):
             # Add more fields as needed
         }
         response = login_user(data)
-        if response['status'] == 'success':  # Adjust this based on the actual API response
-            # Store the user's information in the session
+        if response['status'] == 'success':
             request.session['email'] = data['email']
-            # If the API returns a token, you can store it in the session as well
             request.session['token'] = response.get('token')
             return redirect('home')
         else:
