fd34463c4742c44aaba61b0ed92e6056dbc2a2e5
longqtr26 <longqtr2608@gmail.com>
2024-04-02 16:54:41 +0700

    modified local environment

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'sod', 
             'USER': 'postgres', 
-            'PASSWORD': DB_PASSWORD,
+            'PASSWORD': 181200,
             'HOST': '127.0.0.1', 
             'PORT': '5432',
         },
