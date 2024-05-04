438bd5fbe29398ff30c6fc140739b855c3275e08
sameershaik-coder <sameershaik.coder@gmail.com>
2024-05-04 14:14:09 +0530

    accept pr-11

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,8 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tempkey'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
